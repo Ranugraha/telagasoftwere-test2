f76f227a81939dc746ce141005b8bee1ebfe1300
Ranugrah <awan72.ra@gmail.com>
2022-03-10 14:14:10 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
 </head>
 <body>
     <h1>Hello world</h1>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus provident cum placeat.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus provident cum placeat. </p>
+    <ul>
+        <li>1. Ridwan</li>
+        <li>1. Ridwan</li>
+    </ul>
 </body>
 </html>
